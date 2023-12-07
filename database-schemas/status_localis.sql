844de1d10b549192c84a1ea05013d7ac3637b190
Anastasiia Skrypnykova <anastasiya9983@gmail.com>
2023-12-07 19:40:33 +0000

    Created status_localis table to track health data

--- database-schemas/status_localis.sql
+++ database-schemas/status_localis.sql
@@ -1,1 +1,11 @@
 -- SQL script to create the status_localis table
+CREATE TABLE status_localis (
+    patient_id VARCHAR(255) NOT NULL,
+    date DATETIME NOT NULL,
+    heart_rate FLOAT,
+    respiratory_rate INT,
+    SpO2 INT,
+    temperature FLOAT,
+    status VARCHAR(50) NOT NULL CHECK (status IN ('Normal', 'Abnormal')),
+    FOREIGN KEY (patient_id) REFERENCES general_info(patient_id)
+);
